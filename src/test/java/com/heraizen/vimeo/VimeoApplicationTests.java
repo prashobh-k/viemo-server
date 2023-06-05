48ba96781e9fcc40152b0ec8d69f1c0e6e0938e8
prashobh <prashobhkv@gmail.com>
2023-06-05 16:19:01 +0530

    intial commit

--- src/test/java/com/heraizen/vimeo/VimeoApplicationTests.java
+++ src/test/java/com/heraizen/vimeo/VimeoApplicationTests.java
@@ -1,13 +1,13 @@
-package com.heraizen.vimeo;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class VimeoApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.heraizen.vimeo;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class VimeoApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
